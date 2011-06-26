51e80e8be75eca45713d7e68b24adaf6a43659ca
whiter4bbit <wr4bbit@gmail.com>
2011-06-27 00:16:01 +0300

    removed test subproject

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,6 @@ object Dependencies {
    val scalaz = "org.scalaz" %% "scalaz-core" % "6.0.1"
    val commonsLang = "commons-lang" % "commons-lang" % "2.3"
    val specs2 = "org.specs2" %% "specs2" % "1.3" % "test"
-   val liftJson = "net.liftweb" %% "lift-json-scalaz" % "2.4-SNAPSHOT"
    val casbah = "com.mongodb.casbah" % "casbah_2.9.0-1" % "2.1.5.0"      
 
    val scalatra = "org.scalatra" %% "scalatra" % scalatraVersion   
@@ -17,23 +16,31 @@ object Dependencies {
    val scalate = "org.scalatra" %% "scalatra-scalate" % scalatraVersion
    val scalatest = "org.scalatra" %% "scalatra-scalatest" % scalatraVersion % "test"
    val slf4jBinding = "ch.qos.logback" % "logback-classic" % "0.9.25" % "runtime"
-
-   val specs = "org.specs" % "specs" % "1.4.3" % "test"
-   val jetty = "org.mortbay.jetty" % "jetty" % "6.1.22" % "jetty"
-   val chttp = "whiter4bbit.info" %% "chttp" % "1.0"
 }
 
 object Resolvers {
    lazy val snapshots = "Scala Tools Snapshots" at "http://scala-tools.org/repo-snapshots/"
    lazy val sonatype = "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
    lazy val fuse = "FuseSource Snapshot Repository" at "http://repo.fusesource.com/nexus/content/repositories/snapshots"
+   lazy val whiter4bbit = "whiter4bbit's repository" at "http://whiter4bbit.github.com/maven/"
+   lazy val home = Resolver.file("home repo", file("/home/whiter4bbit/documents/whiter4bbit.github.com/maven/")) transactional()
 
-   val allResolvers = Seq(snapshots, sonatype, fuse)   
+   val allResolvers = Seq(home/*, snapshots*/, sonatype, fuse, whiter4bbit)   
+}
+
+object Publish {
+   val publishRepo = Some(
+      Resolver.file("github", 
+                    new File("/home/whiter4bbit/documents/whiter4bbit.github.com/maven"))(
+		    Patterns(true, Resolver.mavenStyleBasePattern)
+      )      
+   )
 }
 
 object OAuthBuild extends Build {
   import Dependencies._
   import Resolvers._
+  import Publish._
 
   val buildOrganization = "info.whiter4bbit"
   val buildVersion = "1.0"
@@ -42,31 +49,50 @@ object OAuthBuild extends Build {
   val buildSettings = Defaults.defaultSettings ++ Seq (
     organization := buildOrganization,
     version      := buildVersion,
-    scalaVersion := buildScalaVersion
+    scalaVersion := buildScalaVersion,
+    publishTo	 := publishRepo
   )
 
   val coreSettings = Seq (
     resolvers := allResolvers,
-    libraryDependencies := Seq(scalaz, commonsLang, specs2)
+    libraryDependencies := Seq(scalaz, commonsLang, specs2),
+    publishTo := publishRepo
   )
 
   val scalatraSettings = Seq (
     resolvers := allResolvers,
-    libraryDependencies := Seq(scalatra, servletApi, slf4jBinding, liftJson, specs2)
+    libraryDependencies := Seq(scalatra, servletApi, slf4jBinding, specs2),
+    publishTo := publishRepo
   )
 
   val mongoDBStorageSettings = Seq (
     resolvers := allResolvers,
-    libraryDependencies := Seq(casbah)
+    libraryDependencies := Seq(casbah),
+    publishTo := publishRepo
   )
 
-  val exampleProviderSettings = Seq (
-    resolvers := allResolvers,
-    libraryDependencies := Seq(jetty, scalatra, scalatraSpecs2, scalate, servletApi, scalatest, slf4jBinding, casbah, liftJson, scalaz, specs, chttp)
+  lazy val oauth = Project(
+     "oauth", 
+     file("."), 
+     settings = buildSettings
+  ) aggregate (core, scalatraBindings, mongoDBStorage)
+
+  lazy val core = Project(
+     "oauth-core", 
+     file("core") , 
+     settings = buildSettings ++ coreSettings
   )
 
-  lazy val core = Project("oauth-core", file("core") , settings = buildSettings ++ coreSettings)
-  lazy val scalatraBindings = Project("scalatra", file("scalatra"), settings = buildSettings ++ scalatraSettings) dependsOn(core)
-  lazy val mongoDBStorage = Project("oauth-mongodb", file("mongodb"), settings = buildSettings ++ mongoDBStorageSettings) dependsOn(core) 
-  lazy val exampleProvider = Project("example-provider", file("example_provider"), settings = buildSettings ++ webSettings ++ exampleProviderSettings) dependsOn(core, scalatraBindings, mongoDBStorage)
+  lazy val scalatraBindings = Project(
+     "oauth-scalatra", 
+     file("scalatra"), 
+     settings = buildSettings ++ scalatraSettings
+  ) dependsOn(core)
+
+  lazy val mongoDBStorage = Project(
+     "oauth-mongodb", 
+     file("mongodb"), 
+     settings = buildSettings ++ mongoDBStorageSettings
+  ) dependsOn(core) 
+
 }
--- scalatra/src/main/scala/scalatra/scalatraz.scala
+++ scalatra/src/main/scala/scalatra/scalatraz.scala
@@ -2,26 +2,12 @@ package info.whiter4bbit.oauth.scalatra
 
 import scalaz._
 import Scalaz._
-import net.liftweb.json.scalaz.JsonScalaz._
-import net.liftweb.json._
 import org.scalatra._
 import java.util.Date
 
 trait Scalatraz { self: ScalatraKernel =>
    sealed abstract class ScalatrazError(val message: String, val code: Int = 400)
    case class GenericError(override val message: String) extends ScalatrazError(message)
-
-   implicit def dateJSON(implicit formats: Formats): JSON[Date] = new JSON[Date] {
-      def read(json: JValue) = json match {
-         case v@JString(x) => formats.dateFormat.parse(x).map((date) => {
-	    date.success
-	 }).getOrElse(UnexpectedJSONError(v, classOf[JString]).fail.liftFailNel)
-	 case x => UnexpectedJSONError(x, classOf[JString]).fail.liftFailNel
-      }
-      def write(date: Date) = {
-         JString(formats.dateFormat.format(date))
-      }
-   }
 
    def headerz(key: String): Validation[ScalatrazError, String] = {
       val value = self.request.getHeader(key)
